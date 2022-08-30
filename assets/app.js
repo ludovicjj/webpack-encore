8abeb814761bdd1de250620a218e108c9365bb54
ludovicjj <jahanlud@gmail.com>
2022-08-30 15:41:17 +0200

    bootstrap v5.2.0

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,12 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+// bootstrap JS
+import 'bootstrap'
+
 // start the Stimulus application
 //import './bootstrap';
+
+
 import getNiceMessage from './js/get_nice_message';
 console.log(getNiceMessage(5))
